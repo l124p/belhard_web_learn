f04922b29a02de801592f0c1bbf682ad24cca4af
Andry <andry.peshko@gmail.com>
2024-02-19 10:32:33 +0300

    hw3 changed

--- HW3/app.py
+++ HW3/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
-from weather import get_weather
 from datetime import datetime
-
+import asyncio
+from belhard_web_learn.HW3.weather.weather import get_weather, get_weather_async
 
 app = Flask(__name__)
 
@@ -14,11 +14,31 @@ def index():
 
 @app.route('/weather')
 def weather():
-    city = 'minsk'
-    temp = get_weather(city)
-    cur_date = datetime.now().strftime('%d.%m.%y')
-    print(cur_date)
-    return f'<h1>City: {city}</h1> <p>Temp: {temp}</p>'
+    cities = ['minsk', 'brest', 'grodno', 'vitebsk', 'gomel']
+    city = request.args.get('city', 'minsk')
+    out = ''.encode('utf-8')
+    if city == 'Все города':
+        response = asyncio.run(get_weather_async(cities))
+        for item in response:
+            out += f'<p>В городе <b>{item['city']}</b> температура воздуха составляет <b>{item['temp']}</b> градусов</p>'.encode('utf-8')
+        #for city in cities:
+            #response = asyncio.run(get_weather_async(city))
+            #print(response)
+            #out += f'<p>В городе <b>{response['city']}</b> температура воздуха составляет <b>{response['temp']}</b> градусов</p>'.encode('utf-8')
+        with open('weather/index.html', 'rb') as f:
+            data = f.read()
+            data += bytes(out)
+        return data
+    else:
+        response = get_weather(city)
+        cur_date = datetime.now().strftime('%d.%m.%y')
+        print(cur_date)
+        print(response)
+        with open('weather/index.html', 'rb') as f:
+            data = f.read()
+            out = f'<p>В городе <b>{response['city']}</b> температура воздуха составляет <b>{response['temp']}</b> градусов</p>'.encode('utf-8')
+            data += bytes(out)
+        return data
 
 
 @app.route('/news')
